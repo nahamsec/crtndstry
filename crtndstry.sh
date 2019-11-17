9822c107799a598418cd48f26533fe40366500e0
Daniel Roberson <daniel@planethacker.net>
2019-11-16 16:27:47 -0800

    remove unnecessary output

--- crtndstry.sh
+++ crtndstry.sh
@@ -21,23 +21,21 @@ certdata(){
 		certspotter=$(curl -s https://certspotter.com/api/v0/certs\?domain\=$1 | jq '.[].dns_names[]' | sed 's/\"//g' | sed 's/\*\.//g' | sort -u | grep -w $1\$ | tee rawdata/certspotter.txt)
 		#get a list of domains from digicert
 		digicert=$(curl -s https://ssltools.digicert.com/chainTester/webservice/ctsearch/search?keyword=$1 -o rawdata/digicert.json) 
-		echo "$crtsh"
-		echo "$certspotter" 
-		echo "$digicert" 
+		#echo "$crtsh"
+		#echo "$certspotter"
+		#echo "$digicert"
 }
-
 
 
 rootdomains() { #this creates a list of all unique root sub domains
-	clear
-	echo "working on data"
-	cat rawdata/crtsh.txt | rev | cut -d "."  -f 1,2,3 | sort -u | rev | tee ./$1-temp.txt
-	cat rawdata/certspotter.txt | rev | cut -d "."  -f 1,2,3 | sort -u | rev | tee -a ./$1-temp.txt
+	cat rawdata/crtsh.txt | rev | cut -d "."  -f 1,2,3 | sort -u | rev > ./$1-temp.txt
+	cat rawdata/certspotter.txt | rev | cut -d "."  -f 1,2,3 | sort -u | rev >> ./$1-temp.txt
 	domain=$1
-	jq -r '.data.certificateDetail[].commonName,.data.certificateDetail[].subjectAlternativeNames[]' rawdata/digicert.json | sed 's/"//g' | grep -w "$domain$" | rev | cut -d "."  -f 1,2,3 | sort -u | rev | tee -a ./$1-temp.txt
+	jq -r '.data.certificateDetail[].commonName,.data.certificateDetail[].subjectAlternativeNames[]' rawdata/digicert.json | sed 's/"//g' | grep -w "$domain$" | rev | cut -d "."  -f 1,2,3 | sort -u | rev >> ./$1-temp.txt
 	cat $1-temp.txt | sort -u | tee ./data/$1-$(date "+%Y.%m.%d-%H.%M").txt; rm $1-temp.txt
-	echo "Number of domains found: $(cat ./data/$1-$(date "+%Y.%m.%d-%H.%M").txt | wc -l)" 
+	echo "[+] Number of domains found: $(cat ./data/$1-$(date "+%Y.%m.%d-%H.%M").txt | wc -l)"
 }
+
 
 certdata $1
 rootdomains $1
